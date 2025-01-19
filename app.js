2f230ecad63dcc3fd2fa6e7a6ed0fe1296d56a7d
Eri <erysy77@gmail.com>
2025-01-19 15:24:11 -0300

    Adicionando a função de tentativas

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -6,13 +7,31 @@ function exibirTextoNaTela(tag, texto){
 }
 
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100'); 
+
+exibirTextoNaTela('h1', 'Número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10'); 
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
 
-function verificarChute(){
-    console.log(numeroSecreto);
+function verificarChute(){  
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Parabéns! Você Acertou');
+        let palavraTentativa = tentativas > 1 ? 'Tentativas' : 'Tentativa';
+        let mensagemTentativas = `Você acertou o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+    }else{
+        console.log(tentativas);
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        }else{
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+    }
+    
+    
 }
