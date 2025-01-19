505d1b9663384e51f2f4816a330c255d2dd06974
Eri <erysy77@gmail.com>
2025-01-19 12:23:22 -0300

    Adicionando Função com Retorno na linha 12

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -7,7 +9,10 @@ function exibirTextoNaTela(tag, texto){
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100'); 
 
-function verificarChute(){
-    console.log('Click');
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
 
+function verificarChute(){
+    console.log(numeroSecreto);
 }
