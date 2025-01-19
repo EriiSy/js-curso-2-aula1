dcd9b0375693a689be313d1a3db71ca4220df68b
Eri <erysy77@gmail.com>
2025-01-19 12:18:18 -0300

    Funções com Parâmetros

---
+++ README.md
@@ -0,0 +1,5 @@
+Olá, neste código estou aprendendo a mexer em funções com parâmetros em JS.
+
+Obs: HTML e CSS não foram realizados por mim e sim pela Alura. 
+
+Link do Curso: [Lógica de programação: explore funções e listas](https://cursos.alura.com.br/course/logica-programacao-funcoes-listas)
---
+++ app.js
@@ -0,0 +1,13 @@
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100'); 
+
+function verificarChute(){
+    console.log('Click');
+
+}
